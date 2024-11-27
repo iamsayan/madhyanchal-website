eb6fe6c4e1cc9f5cb171a36d296f815260e4da28
Sayan Datta <sdcreativity12@gmail.com>
2024-11-27 13:56:45 +0530

    added changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,12 +4,11 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
     // Get the origin and referer headers
     const origin = request.headers.get('origin')
-    const referer = request.headers.get('referer')
+    //const referer = request.headers.get('referer')
 
     // Add your website's URL here
     const allowedOrigins = [
         process.env.NEXT_PUBLIC_SITE_URL,
-        'http://localhost:3000'
     ]
 
     // Check if the request is for the API route
